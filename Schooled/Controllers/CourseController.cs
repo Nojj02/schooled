7c14238c40d5dd8067f53ac74252d7fa397bcdf7
Nojj02 <nojj02@gmail.com>
2017-12-25 17:21:08 +0800

    Added read endpoint

--- Schooled/Controllers/CourseController.cs
+++ Schooled/Controllers/CourseController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
@@ -7,28 +6,18 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Newtonsoft.Json;
-using Npgsql;
-using NpgsqlTypes;
 
 namespace Schooled.Controllers
 {
     [Route("api/[controller]")]
     public class CourseController : Controller
     {
-        private const string ConnectionString = "Host=localhost;Username=postgres;Password=thepassword;Database=postgres;Search Path=schooled";
-
-        // GET api/values
+        // GET api/course
         [HttpGet]
         public IEnumerable<CourseModel> Get()
         {
-            return new List<CourseModel>
-            {
-                new CourseModel
-                {
-                    Code = "MATH 11"
-                }
-            };
+            var courseRepository = new CourseRepository();
+            return courseRepository.GetAll().Select(x => new CourseModel(x));
         }
 
 //        // GET api/values/5
@@ -38,36 +27,13 @@ namespace Schooled.Controllers
 //            return "value";
 //        }
 
-        // POST api/values
+        // POST api/course
         [HttpPost]
-        public async Task Post(CoursePostModel model)
+        public async Task Post([FromBody]CoursePostModel model)
         {
-            using (var sqlConnection = new NpgsqlConnection(ConnectionString))
-            {
-                sqlConnection.Open();
-                var entity = new Course(name: model.Name, code: model.Code);
-
-                using (var transaction = sqlConnection.BeginTransaction())
-                {
-                    try
-                    {
-                        var command = "INSERT INTO schooled.course (id, content, timestamp) VALUES (@id, @content, @timestamp)";
-                        using (var sqlCommand = new Npgsql.NpgsqlCommand(command, sqlConnection))
-                        {                
-                            sqlCommand.Parameters.AddWithValue("id", NpgsqlDbType.Uuid, Guid.NewGuid());
-                            sqlCommand.Parameters.AddWithValue("content", NpgsqlDbType.Jsonb, JsonConvert.SerializeObject(entity));
-                            sqlCommand.Parameters.AddWithValue("timestamp", NpgsqlDbType.TimestampTZ, DateTime.UtcNow);
-                            sqlCommand.ExecuteNonQuery();
-                        }
-                        await transaction.CommitAsync();
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        await transaction.RollbackAsync();
-                    }
-                }
-            }
+            var entity = new Course(name: model.Name, code: model.Code);
+            var courseRepository = new CourseRepository();
+            await courseRepository.Save(entity);
         }
 //
 //        // PUT api/values/5
@@ -85,6 +51,12 @@ namespace Schooled.Controllers
 
     public class CourseModel
     {
+        public CourseModel(Course course)
+        {
+            Name = course.Name;
+            Code = course.Code;
+        }
+
         public string Name { get; set; }
 
         public string Code { get; set; }
