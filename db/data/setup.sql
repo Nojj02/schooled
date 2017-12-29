25f62fd87684abe7d4b652d45c0edd6abc011d44
Nojj02 <nojj02@gmail.com>
2017-12-29 19:22:12 +0800

    Ignored some other files; Cleanup

--- db/data/setup.sql
+++ db/data/setup.sql
@@ -4,27 +4,6 @@
 CREATE SCHEMA "schooled"
     AUTHORIZATION postgres;
 
--- course table
-CREATE TABLE "schooled"."course"
-(
-    "db_id" bigserial NOT NULL,
-    "id" uuid NOT NULL,
-    "content" jsonb NOT NULL,
-    "timestamp" timestamp with time zone NOT NULL
-    -- "Version" bigint NOT NULL,
-    -- "MessageType" character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    -- "SerializedMessage" json NOT NULL,
-    -- CONSTRAINT "OrderEvent_pkey" PRIMARY KEY ("dbId"),
-    -- CONSTRAINT "UniqueIdVersion" UNIQUE ("Id", "Version")
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE "schooled"."course"
-    OWNER to postgres;
-
 -- registration table
 CREATE TABLE "schooled"."registration"
 (
