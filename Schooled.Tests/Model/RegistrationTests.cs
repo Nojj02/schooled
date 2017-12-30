9fc3ed0c5057cd55911634ac294c8815709e53f5
Nojj02 <nojj02@gmail.com>
2017-12-30 13:58:41 +0800

    Added events

--- Schooled.Tests/Model/RegistrationTests.cs
+++ Schooled.Tests/Model/RegistrationTests.cs
@@ -29,8 +29,10 @@ namespace Schooled.Tests.Model
                         units: 3)
                 };
 
+            var id = Guid.NewGuid();
             var registration =
-                new Registration(Guid.NewGuid(), studentNumber: "016-00125",
+                new Registration(id, 
+                    studentNumber: "016-00125",
                     academicTerm: academicTerm,
                     courses: courses);
             
@@ -38,6 +40,16 @@ namespace Schooled.Tests.Model
             Assert.Equal("2040-1", registration.AcademicTerm.ToString());
             Assert.Equal(1, registration.Courses.Count);
             Assert.Equal("Math11", registration.Courses[0].Code);
+            
+            Assert.Equal(1, registration.Events.Count);
+            var createdEvent = (RegistrationCreatedEvent)registration.Events[0];
+            
+            Assert.Equal(id, createdEvent.Id);
+            Assert.Equal("016-00125", createdEvent.StudentNumber);
+            Assert.Equal("2040-1", createdEvent.AcademicTerm.ToString());
+            Assert.Equal(1, createdEvent.Courses.Count);
+            Assert.Equal("Math11", createdEvent.Courses[0].Code);
+            
         }
         
         [Fact]
--- Schooled/Model/Registration.cs
+++ Schooled/Model/Registration.cs
@@ -6,6 +6,7 @@ namespace Schooled.Model
     public class Registration : Entity
     {
         private readonly List<Course> _courses = new List<Course>();
+        private readonly List<IRegistrationEvent> _events = new List<IRegistrationEvent>();
 
         public Registration(Guid id, string studentNumber, AcademicTerm academicTerm, IEnumerable<Course> courses)
             : base(id)
@@ -13,6 +14,8 @@ namespace Schooled.Model
             StudentNumber = studentNumber;
             AcademicTerm = academicTerm;
             _courses.AddRange(courses);
+            
+            _events.Add(new RegistrationCreatedEvent(this));
         }
 
         public string StudentNumber { get; }
@@ -21,10 +24,36 @@ namespace Schooled.Model
 
         public IReadOnlyList<Course> Courses => _courses;
 
+        public IReadOnlyList<IRegistrationEvent> Events => _events;
+
         public void ChangeCourseSelection(IEnumerable<Course> courses)
         {
             _courses.Clear();
             _courses.AddRange(courses);
         }
+    }
+
+    public class RegistrationCreatedEvent : IRegistrationEvent
+    {
+        public RegistrationCreatedEvent(Registration registration)
+        {
+            Id = registration.Id;
+            StudentNumber = registration.StudentNumber;
+            AcademicTerm = registration.AcademicTerm;
+            Courses = registration.Courses;
+        }
+        
+        public Guid Id { get; }
+
+        public string StudentNumber { get; }
+
+        public AcademicTerm AcademicTerm { get; }
+
+        public IReadOnlyList<Course> Courses { get; }
+    }
+
+    public interface IRegistrationEvent
+    {
+        Guid Id { get; }
     }
 }
